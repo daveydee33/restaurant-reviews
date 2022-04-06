15383d752cbe71087b4871a94523b310741031bf
Dave Degeatano <daveydee33@users.noreply.github.com>
2022-04-06 22:05:43 +0800

    add restaurant button

--- client/src/views/restaurants/index.js
+++ client/src/views/restaurants/index.js
@@ -2,7 +2,16 @@ import { useContext, useEffect } from 'react'
 import { restaurantContext } from '../../utility/context/restaurant/RestaurantState'
 
 import { MoreVertical, Edit, Trash } from 'react-feather'
-import { Table, Badge, UncontrolledDropdown, DropdownMenu, Dropdownrestaurant, DropdownToggle } from 'reactstrap'
+import {
+  Table,
+  Badge,
+  UncontrolledDropdown,
+  DropdownMenu,
+  Dropdownrestaurant,
+  DropdownToggle,
+  Card,
+  Button
+} from 'reactstrap'
 
 const UsersPage = () => {
   const { getRestaurants, restaurants, resetToDefault } = useContext(restaurantContext)
@@ -17,31 +26,37 @@ const UsersPage = () => {
   }
 
   return (
-    <Table hover responsive>
-      <thead>
-        <tr>
-          <th>Restaurant</th>
-          <th>Average Score</th>
-          <th># of Reviews</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {restaurants.results.map(restaurant => {
-          return (
-            <tr key={restaurant.id}>
-              <td>{restaurant.title}</td>
-              <td>{3.5}</td>
-              <td>{'99'}</td>
-              <td>
-                <Edit size={15} className='m-1' />
-                <Trash size={15} className='m-1' />
-              </td>
-            </tr>
-          )
-        })}
-      </tbody>
-    </Table>
+    <>
+      <Button color='primary' className='m-1'>
+        Add Restaurant
+      </Button>
+
+      <Table hover responsive>
+        <thead>
+          <tr>
+            <th>Restaurant</th>
+            <th>Average Score</th>
+            <th># of Reviews</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {restaurants.results.map(restaurant => {
+            return (
+              <tr key={restaurant.id}>
+                <td>{restaurant.title}</td>
+                <td>{3.5}</td>
+                <td>{'99'}</td>
+                <td>
+                  <Edit size={15} className='m-1' />
+                  <Trash size={15} className='m-1' />
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </Table>
+    </>
   )
 }
 
