322d337601bc08d91a35ee2d9ceb7e8822e170c2
Dave Degeatano <daveydee33@users.noreply.github.com>
2022-04-07 19:31:35 +0800

    add star ratings on detail page

--- client/src/views/restaurants/Detail.js
+++ client/src/views/restaurants/Detail.js
@@ -2,6 +2,7 @@ import { useState, useContext, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { Card, CardHeader, CardBody, CardTitle, CardText } from 'reactstrap'
 import { restaurantContext } from '../../utility/context/restaurant/RestaurantState'
+import StarRatings from 'react-star-ratings'
 
 const SecondPage = () => {
   const params = useParams()
@@ -14,20 +15,30 @@ const SecondPage = () => {
   }, [])
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>
-          {id} - {current?.title} 🙌
-        </CardTitle>
-      </CardHeader>
-      <CardBody>
-        <CardText>This is your second page.</CardText>
-        <CardText>
-          Chocolate sesame snaps pie carrot cake pastry pie lollipop muffin. Carrot cake dragée chupa chups jujubes.
-          Macaroon liquorice cookie wafer tart marzipan bonbon. Gingerbread jelly-o dragée chocolate.
-        </CardText>
-      </CardBody>
-    </Card>
+    <>
+      <h1>
+        {id} - {current?.title}
+      </h1>
+      <StarRatings rating={current?.reviewAvg} starRatedColor='gold' starDimension='2rem' starSpacing='0' />
+      <p>Total Reviews: {current?.reviews?.length}</p>
+      <p>Average Score: {current?.reviewAvg}</p>
+
+      {current?.reviews.map(review => {
+        return (
+          <Card key={review._id}>
+            <CardHeader>
+              <CardTitle>
+                <StarRatings rating={review.rating} starRatedColor='gold' starDimension='1rem' starSpacing='0' />
+              </CardTitle>
+            </CardHeader>
+            <CardBody>
+              <CardText>{review.comment}</CardText>
+              <CardText>{new Date(review.dateVisited).toDateString()}</CardText>
+            </CardBody>
+          </Card>
+        )
+      })}
+    </>
   )
 }
 
