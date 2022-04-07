1909ef445c7aa9c9b2b56211bd6b146648c044e2
Dave Degeatano <daveydee33@users.noreply.github.com>
2022-04-07 09:58:05 +0800

    new user add form WIP. need to specify role, and set default val

--- client/src/views/users/Form.js
+++ client/src/views/users/Form.js
@@ -3,10 +3,14 @@ import _cloneDeep from 'lodash/cloneDeep'
 import { X, Plus, Trash } from 'react-feather'
 import CreatableSelect from 'react-select/creatable'
 import Repeater from '@components/repeater'
-import { Modal, ModalBody, Button, Form, FormGroup, Input, Label } from 'reactstrap'
+import { Modal, ModalBody, Button, Form, FormGroup, Input, Label, FormFeedback, Alert } from 'reactstrap'
 import Select from 'react-select'
 import { isObjEmpty, selectThemeColors } from '@utils'
 import '@styles/react/libs/react-select/_react-select.scss'
+import { useForm, Controller } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import InputPasswordToggle from '@components/input-password-toggle'
 
 const ModalHeader = props => {
   const { children, handleFormPanel } = props
@@ -31,11 +35,89 @@ const FormPanel = props => {
     { value: 'admin', label: 'admin' }
   ]
 
+  const defaultValues = {
+    email: 'some@email.com',
+    password: 'aaaaaaa1',
+    role: 'user'
+  }
+
+  // field validations
+  const SignupSchema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup
+      .string()
+      .min(8)
+      .matches(
+        /^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9!@#$%^&*]{8,16}$/,
+        'password must contain at least 1 number and 1 letter'
+      )
+      .required()
+  })
+
+  const {
+    control,
+    setError,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({ defaultValues, mode: 'onChange', resolver: yupResolver(SignupSchema) })
+  const [errorText, setErrorText] = useState('')
+
   const handleSidebarTitle = () => {
     if (!isObjEmpty(selectedItem)) {
-      return 'Edit Item'
+      return 'Edit User'
     } else {
-      return 'Add Item'
+      return 'Add User'
+    }
+  }
+
+  const onSubmit = data => {
+    if (Object.values(data).every(field => field.length > 0)) {
+      const { email, password, role } = data
+
+      try {
+        addItem({ email, password, role: role.value })
+        handleFormPanel()
+      } catch (error) {
+        console.log('ERROR')
+        console.log(error)
+      }
+
+      // useJwt
+      //   .register({ email, password })
+      //   .then(res => {
+      //     if (res.data.error) {
+      //       for (const property in res.data.error) {
+      //         if (res.data.error[property] !== null) {
+      //           setError(property, {
+      //             type: 'manual',
+      //             message: res.data.error[property]
+      //           })
+      //         }
+      //       }
+      //     } else {
+      //       const data = {
+      //         ...res.data.user,
+      //         accessToken: res.data.tokens.access.token,
+      //         refreshToken: res.data.tokens.refresh.token
+      //       }
+      //       // ability.update(res.data.user.ability) // TODO:
+      //       // dispatch(handleLogin(data))
+      //       // navigate('/')
+      //     }
+      //   })
+      //   .catch(err => {
+      //     console.error(err)
+      //     setErrorText(err.response.data.message)
+      //   })
+    } else {
+      for (const key in data) {
+        if (data[key].length === 0) {
+          setError(key, {
+            type: 'manual',
+            message: `Please enter a valid ${key}`
+          })
+        }
+      }
     }
   }
 
@@ -70,8 +152,8 @@ const FormPanel = props => {
             disabled={!email.length}
             className='m-1'
             onClick={() => {
-              updateItem(selectedItem.id, payload)
-              handleFormPanel()
+              // updateItem(selectedItem.id, payload)
+              // handleFormPanel()
             }}
           >
             Update
@@ -103,12 +185,12 @@ const FormPanel = props => {
         <Fragment>
           <Button
             color='primary'
-            disabled={!email.length}
+            // disabled={!email.length}
             className='add-todo-item m-1'
-            onClick={() => {
-              addItem(payload)
-              handleFormPanel()
-            }}
+            // onClick={() => {
+            //   handleSubmit(onSubmit)
+            // }}
+            type='submit'
           >
             Create
           </Button>
@@ -130,7 +212,7 @@ const FormPanel = props => {
       onClosed={handleSidebarClosed}
       modalClassName='modal-slide-in sidebar-todo-modal'
     >
-      <Form id='form-modal-todo' className='todo-modal' onSubmit={e => e.preventDefault()}>
+      <Form action='/' onSubmit={handleSubmit(onSubmit)}>
         <ModalHeader
           // dispatch={dispatch}
           handleFormPanel={handleFormPanel}
@@ -138,34 +220,77 @@ const FormPanel = props => {
           {handleSidebarTitle()}
         </ModalHeader>
         <ModalBody className='flex-grow-1 pb-sm-0 pb-3'>
-          <FormGroup>
-            <Label for='email' className='form-label'>
-              Email <span className='text-danger'>*</span>
+          <div className='mb-1'>
+            <Label className='form-label' for='register-email'>
+              Email
             </Label>
-            <Input id='email' value={email} placeholder='email' onChange={e => setEmail(e.target.value)} />
-          </FormGroup>
-
-          <FormGroup>
-            <Label for='password' className='form-label'>
-              Password <span className='text-danger'>*</span>
+            <Controller
+              id='email'
+              name='email'
+              control={control}
+              render={({ field }) => (
+                <Input type='email' placeholder='john@example.com' invalid={errors.email && true} {...field} />
+              )}
+            />
+            {errors.email ? <FormFeedback>{errors.email.message}</FormFeedback> : null}
+          </div>
+          <div className='mb-1'>
+            <Label className='form-label' for='register-password'>
+              Password
             </Label>
-            <Input id='password' value={password} placeholder='password' onChange={e => setPassword(e.target.value)} />
-          </FormGroup>
+            <Controller
+              id='password'
+              name='password'
+              control={control}
+              render={({ field }) => (
+                <InputPasswordToggle className='input-group-merge' invalid={errors.password && true} {...field} />
+              )}
+            />
+            {errors.password ? <FormFeedback>{errors.password.message}</FormFeedback> : null}
+          </div>
 
-          <FormGroup>
-            <Label for='role' className='form-label'>
+          <div className='mb-1'>
+            <Label className='form-label' for='register-role'>
               Role
             </Label>
-            <Select
+
+            <Controller
               id='role'
-              theme={selectThemeColors}
-              className='react-select'
-              classNamePrefix='select'
-              defaultValue={roleOptions[0]}
-              options={roleOptions}
-              isClearable={false}
+              name='role'
+              control={control}
+              render={({ field }) => (
+                <Select
+                  id='role'
+                  theme={selectThemeColors}
+                  className='react-select'
+                  classNamePrefix='select'
+                  defaultValue={roleOptions[0]}
+                  options={roleOptions}
+                  isClearable={false}
+                  invalid={errors.role && true}
+                  {...field}
+                />
+              )}
             />
-          </FormGroup>
+            {errors.role ? <FormFeedback>{errors.role.message}</FormFeedback> : null}
+          </div>
+
+          {errorText && (
+            <Alert color='danger'>
+              <div className='alert-body'>
+                <span>{errorText}</span>
+              </div>
+            </Alert>
+          )}
+
+          {errorText && (
+            <Alert color='danger'>
+              <div className='alert-body'>
+                {/* <span className='fw-bold'>Error</span> */}
+                <span>{errorText}</span>
+              </div>
+            </Alert>
+          )}
 
           <FormGroup className='my-1'>{renderFooterButtons()}</FormGroup>
         </ModalBody>
