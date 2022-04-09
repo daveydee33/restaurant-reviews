d0c3a4e9b3f05c594cc518b350dc76c50f789c1d
Dave Degeatano <daveydee33@users.noreply.github.com>
2022-04-09 21:57:39 +0800

    remove text on not-authorized page

--- client/src/views/NotAuthorized.js
+++ client/src/views/NotAuthorized.js
@@ -78,10 +78,10 @@ const NotAuthorized = () => {
       <div className='misc-inner p-2 p-sm-3'>
         <div className='w-100 text-center'>
           <h2 className='mb-1'>You are not authorized! 🔐</h2>
-          <p className='mb-2'>
+          {/* <p className='mb-2'>
             The Webtrends Marketing Lab website in IIS uses the default IUSR account credentials to access the web pages
             it serves.
-          </p>
+          </p> */}
           <Button tag={Link} color='primary' className='btn-sm-block mb-1' to={'/'}>
             Back to Home
           </Button>
